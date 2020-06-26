9491cf44cd271ce21c50f246a3e95ed702590890
Hiroki Yokoyama <javawriter.hy@gmail.com>
2020-06-26 17:19:33 +0900

    Initial Commit

---
+++ iOS_TFLite_SingleView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS_TFLite_SingleView
+//
+//  Created by 横山裕樹 on 2020/06/26.
+//  Copyright © 2020 HirokiYokoyama. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS_TFLite_SingleViewTests/iOS_TFLite_SingleViewTests.swift
@@ -0,0 +1,34 @@
+//
+//  iOS_TFLite_SingleViewTests.swift
+//  iOS_TFLite_SingleViewTests
+//
+//  Created by 横山裕樹 on 2020/06/26.
+//  Copyright © 2020 HirokiYokoyama. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_TFLite_SingleView
+
+class iOS_TFLite_SingleViewTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
